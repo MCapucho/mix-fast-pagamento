c65c2b21c4a49c4bef79dffc3b303c9e4f495844
Murylo Capucho <mcapuchoribeiro@gmail.com>
2024-01-27 22:10:48 -0300

    Implementação do projeto

--- app/mix-fast-pagamento/src/main/java/br/com/postech/mixfastpagamento/core/usecase/impl/pagamento/PagamentoGerarQRCodeUseCaseImpl.java
+++ app/mix-fast-pagamento/src/main/java/br/com/postech/mixfastpagamento/core/usecase/impl/pagamento/PagamentoGerarQRCodeUseCaseImpl.java
@@ -4,10 +4,8 @@ import br.com.postech.mixfastpagamento.core.entity.Pagamento;
 import br.com.postech.mixfastpagamento.core.gateway.PagamentoGateway;
 import br.com.postech.mixfastpagamento.core.usecase.interfaces.pagamento.PagamentoGerarQRCodeUseCase;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
-@Log4j2
 @RequiredArgsConstructor
 @Service
 public class PagamentoGerarQRCodeUseCaseImpl implements PagamentoGerarQRCodeUseCase {
@@ -16,7 +14,6 @@ public class PagamentoGerarQRCodeUseCaseImpl implements PagamentoGerarQRCodeUseC
 
     @Override
     public String gerarQRCode(Pagamento pagamento) {
-        log.info("Use Case");
         return pagamentoGateway.gerarQrCode(pagamento);
     }
 }
